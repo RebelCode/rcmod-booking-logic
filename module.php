5f0840679fe037fed8f070d0d6a134540da8df05
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-02 17:16:12 +0200

    Added module main file

---
+++ module.php
@@ -0,0 +1,15 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use RebelCode\Bookings\Module\BookingLogicModule;
+
+define('RC_BOOKING_LOGIC_MODULE_DIR', __DIR__);
+define('RC_BOOKING_LOGIC_MODULE_CONFIG', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+define('RC_BOOKING_LOGIC_MODULE_KEY', 'booking_logic');
+
+return function(ContainerInterface $c) {
+    return new BookingLogicModule(
+        RC_BOOKING_LOGIC_MODULE_KEY,
+        $c->get('container-factory')
+    );
+};
