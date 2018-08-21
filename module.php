77c9cbaf912b0c6230b33c8257537836e61d6670
Miguel Muscat <miguelmuscat93@gmail.com>
2018-08-21 13:05:38 +0200

    Constants defined only once
    This allows the module file to be loaded multiple times to get multiple instances of the module callback.

--- module.php
+++ module.php
@@ -3,10 +3,13 @@
 use Psr\Container\ContainerInterface;
 use RebelCode\Bookings\Module\BookingLogicModule;
 
-define('RC_BOOKING_LOGIC_MODULE_DIR', __DIR__);
-define('RC_BOOKING_LOGIC_MODULE_CONFIG', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
-define('RC_BOOKING_LOGIC_MODULE_SERVICES', __DIR__ . DIRECTORY_SEPARATOR . 'services.php');
-define('RC_BOOKING_LOGIC_MODULE_KEY', 'booking_logic');
+// Define constants only once
+if (!defined('RC_BOOKING_LOGIC_MODULE_KEY')) {
+    define('RC_BOOKING_LOGIC_MODULE_KEY', 'booking_logic');
+    define('RC_BOOKING_LOGIC_MODULE_DIR', __DIR__);
+    define('RC_BOOKING_LOGIC_MODULE_CONFIG', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+    define('RC_BOOKING_LOGIC_MODULE_SERVICES', __DIR__ . DIRECTORY_SEPARATOR . 'services.php');
+}
 
 return function(ContainerInterface $c) {
     return new BookingLogicModule(
