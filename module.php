ffb4b53ece76ad38dd363e281689e0bb0ada2d89
Miguel Muscat <miguelmuscat93@gmail.com>
2018-09-13 13:11:45 +0200

    Brought module file up to standard

--- module.php
+++ module.php
@@ -3,18 +3,15 @@
 use Psr\Container\ContainerInterface;
 use RebelCode\Bookings\Module\BookingLogicModule;
 
-// Define constants only once
-if (!defined('RC_BOOKING_LOGIC_MODULE_KEY')) {
-    define('RC_BOOKING_LOGIC_MODULE_KEY', 'booking_logic');
-    define('RC_BOOKING_LOGIC_MODULE_DIR', __DIR__);
-    define('RC_BOOKING_LOGIC_MODULE_CONFIG', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
-    define('RC_BOOKING_LOGIC_MODULE_SERVICES', __DIR__ . DIRECTORY_SEPARATOR . 'services.php');
-}
-
-return function(ContainerInterface $c) {
+return function (ContainerInterface $c) {
     return new BookingLogicModule(
-        RC_BOOKING_LOGIC_MODULE_KEY,
-        [],
+        [
+            'key'                => 'booking_logic',
+            'dependencies'       => [],
+            'module_path'        => __DIR__,
+            'config_file_path'   => __DIR__ . '/config.php',
+            'services_file_path' => __DIR__ . '/services.php',
+        ],
         $c->get('config_factory'),
         $c->get('container_factory'),
         $c->get('composite_container_factory')
